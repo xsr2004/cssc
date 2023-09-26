daa8673cfda167a533712677ff10377c10ce23a1
xsr2004 <113783793+xsr2004@users.noreply.github.com>
2023-09-26 20:35:47 +0800

    项目初始化

---
+++ src/main/java/com/essc/A/dataModel/DO/user.java
@@ -0,0 +1,8 @@
+package com.essc.A.dataModel.DO;
+
+/**
+ * @Author：xsr
+ * @Date：2023/9/26 0:16
+ */
+public class user {
+}
---
+++ src/main/java/com/essc/Common/CsscApplication.java
@@ -0,0 +1,13 @@
+package com.essc.Common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsscApplication.class, args);
+    }
+
+}
